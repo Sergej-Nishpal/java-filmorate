6d8957c75b9c094d40d787218d388793cd32b68c
Sergej Nishpal <sergej.nishpal@yandex.ru>
2022-07-21 22:08:41 +0300

    fix: в ErrorResponse - изменить порядок указания аннотаций (по длине их названия)

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
@@ -3,8 +3,8 @@ package ru.yandex.practicum.filmorate.exception;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
+@RequiredArgsConstructor
 public class ErrorResponse {
     private final String error;
 }
