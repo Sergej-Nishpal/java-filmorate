0bcfd86ab73894d8f572be5a414d663529acba10
Sergej Nishpal <sergej.nishpal@yandex.ru>
2022-08-11 03:16:30 +0300

    feat: добавлен пример интеграционного теста из ТЗ (с поправками - без них не работает)

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,24 +1,30 @@
 package ru.yandex.practicum.filmorate;
 
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
+import lombok.RequiredArgsConstructor;
+import ru.yandex.practicum.filmorate.model.User;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.controller.UserController;
-
+import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class FilmorateApplicationTests {
-
-	@Autowired
-	UserController userController;
-	@Autowired
-	FilmController filmController;
+	private final UserDbStorage userStorage;
 
 	@Test
-	void contextLoads() {
-		assertThat(userController).isNotNull();
-		assertThat(filmController).isNotNull();
+	void testFindUserById() {
+
+		Optional<User> userOptional = Optional.ofNullable(userStorage.getUserById(1));
+
+		assertThat(userOptional)
+				.isPresent()
+				.hasValueSatisfying(user ->
+						assertThat(user).hasFieldOrPropertyWithValue("id", 1L)
+				);
 	}
 }
