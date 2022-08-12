2aada71d8d7f562d3858eb89b890d832737cf6b5
Sergej Nishpal <sergej.nishpal@yandex.ru>
2022-08-12 12:10:00 +0300

    fix: исправить проблему "невидимости" файлов schema.sql и data.sql, проверить работу приложения, убрать ненужную зависимость

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 import java.time.LocalDate;
@@ -13,30 +12,10 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmorateApplicationTests {
-
-	private final JdbcTemplate jdbcTemplate;
 	private final UserDbStorage userStorage;
-
-	@Test
-	void deleteUsers() {
-		String deleteUsersQuery = "DELETE FROM USERS WHERE USER_ID > 0";
-		jdbcTemplate.update(deleteUsersQuery);
-
-		String restartUsersFromOneQuery = "ALTER TABLE USERS ALTER COLUMN USER_ID RESTART WITH 1";
-		jdbcTemplate.update(restartUsersFromOneQuery);
-	}
-
-	@Test
-	void deleteFilms() {
-		String deleteFilmsQuery = "DELETE FROM FILMS WHERE FILM_ID > 0";
-		jdbcTemplate.update(deleteFilmsQuery);
-
-		String restartFilmsFromOneQuery = "ALTER TABLE FILMS ALTER COLUMN FILM_ID RESTART WITH 1";
-		jdbcTemplate.update(restartFilmsFromOneQuery);
-	}
 
 	@Test
 	void testCreateUser() {
