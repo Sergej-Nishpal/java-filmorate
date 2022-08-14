4b6401e67108a6c83d492bee7016a5a7b2e6e5d1
Sergej Nishpal <sergej.nishpal@yandex.ru>
2022-08-14 20:42:28 +0300

    fix: поправить тест, подготовить код для проверки

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,6 +1,11 @@
 package ru.yandex.practicum.filmorate;
 
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.yandex.practicum.filmorate.controller.FilmController;
@@ -9,16 +14,26 @@ import ru.yandex.practicum.filmorate.controller.UserController;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FilmorateApplicationTests {
 
     @Autowired
     UserController userController;
     @Autowired
     FilmController filmController;
+    @Autowired
+    FilmController mpaController;
+    @Autowired
+    FilmController genreController;
 
     @Test
+    @Order(1)
     void contextLoads() {
         assertThat(userController).isNotNull();
         assertThat(filmController).isNotNull();
+        assertThat(mpaController).isNotNull();
+        assertThat(genreController).isNotNull();
     }
 }
