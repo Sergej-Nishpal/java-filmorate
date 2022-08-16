8300c0ae57e2a7ddd929508207ae4d1536f646d9
Sergej Nishpal <sergej.nishpal@yandex.ru>
2022-08-16 18:12:56 +0300

    fix: отделил константу от других полей.

--- src/main/java/ru/yandex/practicum/filmorate/storage/friendship/InMemoryFriendshipStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/friendship/InMemoryFriendshipStorage.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 @Component
 public class InMemoryFriendshipStorage implements FriendshipStorage {
     private static final String USER_ID_NOT_FOUND = "Пользователь с id %d не найден";
+
     private final UserStorage userStorage;
 
     public InMemoryFriendshipStorage(@Qualifier("inMemoryUserStorage") UserStorage userStorage) {
